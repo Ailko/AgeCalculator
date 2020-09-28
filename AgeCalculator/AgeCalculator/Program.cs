6d96ae59f60d496cb5631af0d6f2a187a74abf15
samuel <samuelbarkley2001@gmail.com>
2020-09-28 13:35:39 +0200

    Implemented age calculator

--- AgeCalculator/AgeCalculator/Program.cs
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -7,12 +7,23 @@ namespace AgeCalculator
         static void Main(string[] args)
         {
             ShowWelcomeMessage();
+
+            
+            
         }
 
         static void ShowWelcomeMessage()
         {
             Console.WriteLine("Welkom!! \nDeze applicatie berekent hoe oud je bent. " +
                 "\nHiervoor zullen we uw leeftijd vragen (die enkel voor deze functie gebruikt wordt)");
+        }
+
+        static TimeSpan AgeCalculator(DateTime birthDay)
+        {
+            TimeSpan age = new TimeSpan();
+            age = DateTime.Now - birthDay;
+
+            return age;
         }
     }
 }
