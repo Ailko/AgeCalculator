30c1762930a9e03bab8501df0266858bed2f7edd
Ailko <s113170@ap.be>
2020-09-28 13:23:00 +0200

    Implemented EnterbirthDate

--- AgeCalculator/AgeCalculator/Program.cs
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -14,5 +14,35 @@ namespace AgeCalculator
             Console.WriteLine("Welkom!! \nDeze applicatie berekent hoe oud je bent. " +
                 "\nHiervoor zullen we uw leeftijd vragen (die enkel voor deze functie gebruikt wordt)");
         }
+
+        static DateTime EnterAge()
+        {
+            int year = askX("year");
+            int month = askX("month");
+            int day = askX("day");
+
+            return new DateTime(year, month, day);
+        }
+
+        static int askX(string toAsk)
+        {
+            bool entered = false;
+            while (!entered)
+            {
+                Console.Clear();
+                Console.Write($"Please enter your birth {toAsk}: ");
+                if (Int32.TryParse(Console.ReadLine(), out int value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.ReadLine();
+                }
+            }
+            return 0;
+        }
     }
 }
